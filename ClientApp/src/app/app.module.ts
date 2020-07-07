3cdb2c5f5180a8e59a7ed13042d2aa678b01ef59
romagritsuk <30575084+romagritsuk@users.noreply.github.com>
2020-07-08 01:03:13 +0300

    added sidebar files

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 @NgModule({
     imports:      [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent ],
+    declarations: [ AppComponent, SidebarComponent ],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
