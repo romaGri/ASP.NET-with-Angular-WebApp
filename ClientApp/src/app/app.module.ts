4463c9f136c82141a82cdd8ff5e6243972342ad9
romagritsuk <30575084+romagritsuk@users.noreply.github.com>
2020-07-08 01:45:06 +0300

    added navbar

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
     imports:      [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent, SidebarComponent ],
+    declarations: [ AppComponent, SidebarComponent, NavbarComponent ],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
