b1aa30e432b488d15a634142726179cd4d55974c
romagritsuk <30575084+romagritsuk@users.noreply.github.com>
2020-07-10 23:34:39 +0300

    added app.component styles

--- ClientApp/src/app/navbar/navbar.component.spec.ts
+++ ClientApp/src/app/navbar/navbar.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
 
+import "@types/jest"
+
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
--- ClientApp/src/app/sidebar/sidebar.component.spec.ts
+++ ClientApp/src/app/sidebar/sidebar.component.spec.ts
@@ -20,6 +20,6 @@ describe('SidebarComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+     expect(component).toBeTruthy();
   });
 });
