81ef34dacbdb35544e43de6a5d791717de23584c
romagritsuk <30575084+romagritsuk@users.noreply.github.com>
2020-07-11 17:07:51 +0300

    added first test chart

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -13,8 +13,9 @@ import { from } from 'rxjs';
 import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
 import { LineChartComponent } from './charts/line-chart/line-chart.component';
 import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
+import { ChartsModule } from 'ng2-charts';
 @NgModule({
-    imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+    imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes), ChartsModule],
     declarations: [AppComponent, SidebarComponent, NavbarComponent, SectionSalesComponent, SectionOrdersComponent, SectionHelthComponent, BarChartComponent, LineChartComponent, PieChartComponent],
     bootstrap: [AppComponent]
 })
