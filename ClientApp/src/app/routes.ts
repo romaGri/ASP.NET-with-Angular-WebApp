14b3b765c3c48066a8935332067bd649c5e31d12
romagritsuk <30575084+romagritsuk@users.noreply.github.com>
2020-07-11 03:39:19 +0300

    added routs to section items in sidebar

--- ClientApp/src/app/routes.ts
+++ ClientApp/src/app/routes.ts
@@ -1,5 +1,14 @@
 import { Routes } from '@angular/router';
+import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
+import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
+import { SectionHelthComponent } from './sections/section-helth/section-helth.component';
 
 export const appRoutes: Routes = [
-    
+    { path: 'sales', component: SectionSalesComponent },
+    { path: 'orders', component: SectionOrdersComponent },
+    { path: 'helth', component: SectionHelthComponent },
+
+    //def path
+    { path: '', redirectTo: '/sales', pathMatch: 'full' }
+
 ]; 
